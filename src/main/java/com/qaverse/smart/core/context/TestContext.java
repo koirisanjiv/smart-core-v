05b8110d18911abbb7e183c181e09c3976563bd9
Sanjiv Koiri <sanjiv.koiri@uffizio.com>
2026-05-03 23:08:36 +0530

    updated

--- src/main/java/com/qaverse/smart/core/context/TestContext.java
+++ src/main/java/com/qaverse/smart/core/context/TestContext.java
@@ -1,30 +1,36 @@
 package com.qaverse.smart.core.context;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.openqa.selenium.WebDriver;
 
 public class TestContext {
 
-    private static final ThreadLocal<String> currentTest = new ThreadLocal<>();
-
-    public static void set(String testName) {
-        currentTest.set(testName);
-    }
-
-    public static String get() {
-        return currentTest.get();
-    }
-
-    public static void clear() {
-        currentTest.remove();
-    }
-    
     private WebDriver driver;
 
+    private final Map<String, Object> data = new HashMap<>();
+
+    // ---------------- Driver ----------------
     public WebDriver getDriver() {
         return driver;
     }
 
     public void setDriver(WebDriver driver) {
         this.driver = driver;
+    }
+
+    // ---------------- Generic Context ----------------
+    public void set(String key, Object value) {
+        data.put(key, value);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T get(String key) {
+        return (T) data.get(key);
+    }
+
+    public boolean contains(String key) {
+        return data.containsKey(key);
     }
 }
