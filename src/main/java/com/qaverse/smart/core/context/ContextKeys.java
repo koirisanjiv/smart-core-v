0ff5f2b116aa4596c931b27196618cd55e45068a
Sanjiv Koiri <sanjiv.koiri@uffizio.com>
2026-05-03 23:33:42 +0530

    updated

---
+++ src/main/java/com/qaverse/smart/core/context/ContextKeys.java
@@ -0,0 +1,10 @@
+package com.qaverse.smart.core.context;
+
+public final class ContextKeys {
+
+    private ContextKeys() {} // prevent instantiation
+
+    public static final String DRIVER = "driver";
+    public static final String RETRY_COUNT = "retryCount";
+    public static final String RETRY_REQUIRED = "retryRequired";
+}
--- src/main/java/com/qaverse/smart/core/context/TestContext.java
+++ src/main/java/com/qaverse/smart/core/context/TestContext.java
@@ -3,39 +3,28 @@ package com.qaverse.smart.core.context;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.openqa.selenium.WebDriver;
-
 public class TestContext {
-
-    private WebDriver driver;
 
     private final Map<String, Object> data = new HashMap<>();
 
-    // ---------------- Driver ----------------
-    public WebDriver getDriver() {
-        return driver;
-    }
+    private String testName;
 
-    public void setDriver(WebDriver driver) {
-        this.driver = driver;
-    }
-
-    // ---------------- Generic Context ----------------
-    public void set(String key, Object value) {
-        data.put(key, value);
-    }
-
-    @SuppressWarnings("unchecked")
+    // Generic getter
     public <T> T get(String key) {
         return (T) data.get(key);
     }
 
+    // Generic setter
+    public void set(String key, Object value) {
+        data.put(key, value);
+    }
+
+    // Optional helpers
     public boolean contains(String key) {
         return data.containsKey(key);
     }
-    
-    private String testName;
 
+    // Test name support
     public String getTestName() {
         return testName;
     }
